fdf886ad6278500002fb0d123488682bfd7e3c3e
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-15 14:03:20 +0000

    ヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
-    devise_for :customer, skip:[:passwords], controllers:{
+  
+  # customer用
+  devise_for :customer, skip:[:passwords], controllers:{
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
   
+  
+  
+  
+  # admin用
   devise_for :admin, skip:[:registrations, :passwords], controllers:{
     sessions: "admin/sessions"
   }
-
+  
+  namespace :admin do
+    get 'home/top' => 'homes#top', as: "top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
