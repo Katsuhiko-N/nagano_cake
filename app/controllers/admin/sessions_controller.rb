f6eb4a3c12c736eb06f06aa31042fcbcc35cc836
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-16 10:07:09 +0000

    ログイン関係修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,8 +3,12 @@
 class Admin::SessionsController < Devise::SessionsController
   
   def after_sign_in_path_for(resouce)
-    
+    root_path
+  end
   
+  def after_sign_out_path_for(resouce)
+    root_path
+  end
   
   
   # before_action :configure_sign_in_params, only: [:create]
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  root to: 'homes#top'
+  
   # customer用
   devise_for :customer, skip:[:passwords], controllers:{
     registrations: "public/registrations",
@@ -8,14 +10,10 @@ Rails.application.routes.draw do
   
   
   
-  
   # admin用
   devise_for :admin, skip:[:registrations, :passwords], controllers:{
     sessions: "admin/sessions"
   }
   
-  namespace :admin do
-    get 'home/top' => 'homes#top', as: "top"
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
