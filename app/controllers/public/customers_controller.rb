a7368b92cdef3527e8bc3062dfae0b8a85ae2dc1
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-16 14:27:02 +0000

    顧客マイページ作成開始

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,14 @@
 class Public::CustomersController < ApplicationController
     def show
-        
+        @customer = Customer.find(current_user.id)
+    end
+    
+    
+    private
+    
+    def customer_params
+        params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
+      :postal_code, :address, :telephone_number)
     end
     
 end
