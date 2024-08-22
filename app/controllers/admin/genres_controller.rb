9730420d59af57e8080ed09acc91a7764f84af12
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-22 11:25:28 +0000

    ジャンル機能作成開始

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,32 @@
 class Admin::GenresController < ApplicationController
+    def index
+        @genres = Genre.all
+        @genre = Genre.new
+    end
+    
+    def create
+        
+    end
+    
+    def show
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    
+    private
+    
+    def index_params
+        params.require(:genre).parmit(:name)
+    end
+    
+    
+    
 end
