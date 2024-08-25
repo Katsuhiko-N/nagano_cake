448d65c73a80ae2ce0ba8265ac77125c5eb1b74d
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-08-25 12:51:50 +0000

    ジャンルルーティング修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -13,11 +13,17 @@ class Admin::GenresController < ApplicationController
     end
     
     def edit
-        
+        @genre = Genre.find(params[:id])
     end
     
     def update
-        
+        @genre = Genre.find(params[:id])
+        @genre.id = params[:id]
+        if @genre.update(genre_params)
+            redirect_to admin_genres_path
+        else
+            render :edit
+        end
     end
     
     
