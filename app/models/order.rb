fbe42c5480b5a4e24b719c7ce8bc63c50855760b
Katsuhiko-N <173056385+Katsuhiko-N@users.noreply.github.com>
2024-11-10 09:25:35 +0000

    [Add]enumを定義

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,9 @@ class Order < ApplicationRecord
     
     has_many :order_details, dependent: :destroy
     belongs_to :customer
+    
+    # emum
+    enum payment_method: { credit_card: 0, transfer: 1 }
+    
+    
 end
